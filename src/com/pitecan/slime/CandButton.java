b37b2297727f8c4464b97b356b28c2a6ebc91786
Toshiyuki Masui <masui@pitecan.com>
2011-11-20 21:37:09 +0900

    セッタを簡潔に

--- src/com/pitecan/slime/CandButton.java
+++ src/com/pitecan/slime/CandButton.java
@@ -6,17 +6,11 @@ public class CandButton {
     public int x,y,w,h;
     String text;
     boolean visible;
+    public void setText(String _text){ text = _text; }
+    public void setVisible(boolean _visible){ visible = _visible; }
 
     public CandButton() {
 	text = "";
 	visible = false;
-    }
-
-    public void setText(String _text){
-	text = _text;
-    }
-
-    public void setVisible(boolean _visible){
-	visible = _visible;
     }
 }
