53eca7654979f954191fc3d2647457b9730439b0
Toshiyuki Masui <masui@pitecan.com>
2011-11-28 14:58:53 +0900

    "-"を追加 Google Suggestにtry-catchを追加

--- src/com/pitecan/slime/GoogleSuggest.java
+++ src/com/pitecan/slime/GoogleSuggest.java
@@ -28,32 +28,36 @@ class GoogleSuggest {
 
 	String text = new String();
 
-	DefaultHttpClient httpClient = new DefaultHttpClient();
-        httpClient.getParams().setParameter("http.protocol.content-charset", "UTF-8");
-
-        HttpGet request = new HttpGet(urlstr);
-	HttpResponse httpResponse = null;
 	try {
-	    httpResponse = httpClient.execute(request);
-	} catch (Exception e) {
-	    Log.d("HttpSampleActivity", "Error Execute");
-	}
-	int status = httpResponse.getStatusLine().getStatusCode();
-	if (HttpStatus.SC_OK == status) {
+	    DefaultHttpClient httpClient = new DefaultHttpClient();
+	    httpClient.getParams().setParameter("http.protocol.content-charset", "UTF-8");
+
+	    HttpGet request = new HttpGet(urlstr);
+	    HttpResponse httpResponse = null;
 	    try {
-		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-		text = EntityUtils.toString(httpResponse.getEntity(), "UTF-8"); // これが大事らしいが...
+		httpResponse = httpClient.execute(request);
 	    } catch (Exception e) {
-		Log.d("Slime HttpSampleActivity", "Error");
+		Log.d("HttpSampleActivity", "Error Execute");
 	    }
-	} else {
-	    Log.d("Slime HttpSampleActivity", "Status" + status);
-	}
-	Pattern pat = Pattern.compile("suggestion data=\"([^\"]*)\"/>"); // Google Suggestのフォーマット
-	Matcher matcher = pat.matcher(text);
-	while(matcher.find() && nsuggest < maxSuggestions){
-	    Log.v("Slime","matcher.group(1) = "+matcher.group(1));
-	    suggestions[nsuggest++] = matcher.group(1);
+	    int status = httpResponse.getStatusLine().getStatusCode();
+	    if (HttpStatus.SC_OK == status) {
+		try {
+		    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+		    text = EntityUtils.toString(httpResponse.getEntity(), "UTF-8"); // これが大事らしいが...
+		} catch (Exception e) {
+		    Log.d("Slime HttpSampleActivity", "Error");
+		}
+	    } else {
+		Log.d("Slime HttpSampleActivity", "Status" + status);
+	    }
+	    Pattern pat = Pattern.compile("suggestion data=\"([^\"]*)\"/>"); // Google Suggestのフォーマット
+	    Matcher matcher = pat.matcher(text);
+	    while(matcher.find() && nsuggest < maxSuggestions){
+		Log.v("Slime","matcher.group(1) = "+matcher.group(1));
+		suggestions[nsuggest++] = matcher.group(1);
+	    }
+	} catch (Exception e){
+	    Log.v("Slime","GoogleSuggest error");
 	}
 
 	suggestions[nsuggest] = "";
