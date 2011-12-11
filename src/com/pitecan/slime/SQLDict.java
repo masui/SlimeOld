53790b1c0a9fc232fbdca278d6c1b8e18e0aeecd
Toshiyuki Masui <masui@pitecan.com>
2011-12-11 19:03:25 +0900

    SQLDict.javaにコメント追加

--- src/com/pitecan/slime/SQLDict.java
+++ src/com/pitecan/slime/SQLDict.java
@@ -1,3 +1,10 @@
+//
+// 学習辞書をSQLiteで実装する
+//
+// そもそも学習辞書をSQLiteで実装すべきなのかよくわからないし、
+// 仕様が色々面倒なのだがとりあえず...
+// (2011/12/10)
+//
 package com.pitecan.slime;
 
 import java.util.regex.Pattern;
@@ -44,8 +51,9 @@ public class SQLDict
 		   );
     }
 	
-    public void add(String word, String pat){
-	// 最初に全部消してからinsertすること
+    public void add(String word, String pat){ // エントリ追加
+	// 最初に全部消す
+	db.delete("history", "word = '"+word+"' AND pat = '"+pat+"'", null);
 	int patind = Dict.patInd(pat);
 	db.execSQL("insert into history(word,pat,patind,date) values ('"+word+"', '"+pat+"', "+patind+", datetime('now', 'localtime'));");
     }
@@ -60,17 +68,17 @@ public class SQLDict
 	    cursor.moveToPosition(max);
 	    word = cursor.getString(0);
 	    pat = cursor.getString(1);
-	    Log.v("SQLite","delete -> " + word);
+	    //Log.v("SQLite","delete -> " + word);
 	    db.delete("history", "word = '"+word+"' AND pat = '"+pat+"'", null);
 	}
 	cursor.close();
     }
 
-    public String[][] match(String pat, boolean exactMode){
+    public String[][] match(String pat, boolean exactMode){ // 新しいものから検索
 	ArrayList<String> words = new ArrayList<String>();
 	ArrayList<String> wordpats = new ArrayList<String>();
 	Pattern pattern = (exactMode ? Pattern.compile("^"+pat) : Pattern.compile("^"+pat+".*"));
-	Log.v("Slime","pattern="+pattern);
+	//Log.v("Slime","pattern="+pattern);
 
 	Cursor cursor = db.query("history", new String[] { "word", "pat", "date" },
 				 "patind = " + Dict.patInd(pat), null, null, null, "date desc");
@@ -78,16 +86,16 @@ public class SQLDict
         while (isEof) {
 	    String word = cursor.getString(0);
 	    String wordpat = cursor.getString(1);
-	    Log.v("Slime",String.format("word:%s wordpat:%s\r\n", word, wordpat));
+	    //Log.v("Slime",String.format("word:%s wordpat:%s\r\n", word, wordpat));
 	    if(pattern.matcher(wordpat).matches()){
-		Log.v("Slime/SQLite - match",String.format("word:%s pat:%s\r\n", word, wordpat));
+		//Log.v("Slime/SQLite - match",String.format("word:%s pat:%s\r\n", word, wordpat));
 		words.add(word);
 		wordpats.add(wordpat);
 	    }
             isEof = cursor.moveToNext();
         }
         cursor.close();
-	Log.v("Slime","length = "+words.size());
+	//Log.v("Slime","length = "+words.size());
 	String[][] res = new String[words.size()][2];
 	for(int i=0;i<words.size();i++){
 	    res[i][0] = words.get(i);
